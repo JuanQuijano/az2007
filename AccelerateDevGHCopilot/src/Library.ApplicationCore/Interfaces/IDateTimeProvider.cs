dd6a9a6826e424a0a4a3c67cea4f2e0ea2158697
Juan Quijano <jc_quijano@hotmail.com>
2026-01-29 17:34:50 +0100

    reinicio

--- AccelerateDevGHCopilot/src/Library.ApplicationCore/Interfaces/IDateTimeProvider.cs
+++
@@ -1,9 +0,0 @@
-namespace Library.ApplicationCore;
-
-/// <summary>
-/// Abstraction for date/time operations to enable testability.
-/// </summary>
-public interface IDateTimeProvider
-{
-    DateTime Now { get; }
-}
--- AccelerateDevGHCopilot/src/Library.ApplicationCore/Services/SystemDateTimeProvider.cs
+++
@@ -1,9 +0,0 @@
-namespace Library.ApplicationCore.Services;
-
-/// <summary>
-/// Default implementation of IDateTimeProvider that returns the current system time.
-/// </summary>
-public class SystemDateTimeProvider : IDateTimeProvider
-{
-    public DateTime Now => DateTime.Now;
-}
